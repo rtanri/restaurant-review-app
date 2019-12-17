f0598e3e30f2bbaf1ed5e0b123f9f7d83251ae60
Revin Tanri <tanrirevin@gmail.com>
2019-12-17 23:08:09 +0800

    Add the Alternate text in restaurant images

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  // image.alt = restaurants.name + ' Restaurant';
+  image.alt = restaurant.name + ' Restaurant';
   li.append(image);
 
   const name = document.createElement('h1');
